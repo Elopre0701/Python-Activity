439605297c37237901c4988e916e2dcd4d4f9869
Jhon Elopre <143986904+Elopre0701@users.noreply.github.com>
2024-09-07 03:44:41 +0800

    Add files via upload

---
+++ Grp5_Elopre_ex1.py
@@ -0,0 +1,27 @@
+# My first simple script
+print("\nHello, I'm Jhon Elopre! BSCS 3F\n")
+
+edad = 21
+name = "Jhon Elopre"
+
+print("Name: ", name)
+print("Age: ", edad,"\n")
+
+
+angela = "Angela"
+irene = "Irene"
+judith = "Judith"
+diana = "Diana"
+
+print("Member 1: ", angela)
+print("Member 2: ", irene)
+print("Member 3: ", judith)
+print("Member 4: ", diana, "\n")    
+
+edad, name, gender = 21, "Jhon Elopre", "Male"
+print("Name:", name)
+print("Age:", edad)
+print("Gender:", gender, "\n")
+
+age, edad, num = 21, 21, 21
+print(age, edad, num, "\n")
